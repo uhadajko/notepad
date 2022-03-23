b796f4e58887b819b81c7293807115ec1e527fea
uhadaj <uhadaj@gmail.com>
2022-03-23 20:25:29 +0200

    post class

---
+++ post.rb
@@ -0,0 +1,35 @@
+class Post
+  def initialize
+    # створюємо клас з двома полями
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+    # todo
+  end
+
+  def to_strings
+    # todo
+  end
+
+  # Метод який дозволяє себе зберігати
+  def save
+    file = File.new (file_path, "w:UTF-8")
+
+    for item in strings do
+      file.puts(item)
+    end
+
+    file.close
+  end
+
+  # Метод який визначає назву файлу та шлях до нього
+  def file_path
+    current_puth = File.dirname (__FILE__)
+
+    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+
+    return current_puth + "/" + file_name
+  
+end
